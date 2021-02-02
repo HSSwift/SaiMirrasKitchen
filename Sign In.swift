2ded8f0a57a742e1ed6666ae274279379726ac64
HSSwift <76495346+HSSwift@users.noreply.github.com>
2021-02-01 20:11:37 -0500

    Add files via upload

---
+++ Sign In.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct SignIn: View {
+    var body: some View {
+	NavigationView{
+	VStack{
+           Text("Welcome")
+		.font(.title)
+		
+	
+           
+        }
+    }
+
+}
+
+
+struct SignIn_Previews: PreviewProvider {
+    static var previews: some View {
+        SignIn()
+    }
+}
