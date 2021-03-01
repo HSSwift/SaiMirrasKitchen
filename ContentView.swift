436b527c994923f1742f88b53d1641f8370b5b83
HSSwift <76495346+HSSwift@users.noreply.github.com>
2021-03-01 17:59:59 +0530

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -6,17 +6,41 @@
 //
 
 import SwiftUI
+import GoogleSignIn
 
 struct ContentView: View {
 
     var body: some View {
         NavigationView{
             VStack{
+                
+                
+                Button(action: {
+                    
+                    GIDSignIn.sharedInstance()?
+                        .presentingViewController =
+                        UIApplication.shared.windows.first?
+                        .rootViewController
+                    
+                    GIDSignIn.sharedInstance()?.signIn()
+                    
+                }) {
+                    
+                    Text("Sign In With Google")
+                        .foregroundColor(.white)
+                        .fontWeight(.ultraLight)
+                        .padding(.vertical,10)
+                        .padding(.horizontal,45)
+                        .background(Color.black)
+                        .clipShape(Capsule ())
+                }
+                
                 Image(systemName: "flame")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     .padding()
+                
                 
                 NavigationLink(
                     destination: menu(),
